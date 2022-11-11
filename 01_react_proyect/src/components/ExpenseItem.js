199aa0659d83192a1d48a9eaf8f6f7debc9312ba
Carlos Fuentes <char_basket@hotmail.com>
2022-11-11 15:41:29 +0100

    New Expense Component

--- 01_react_proyect/src/components/ExpenseItem.js
+++ 01_react_proyect/src/components/ExpenseItem.js
@@ -1,21 +1,17 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  // It´s always better to have the logic outside the return
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
+  // It´s always better to have the logic outside the return.
+  // It´s better to make another component
 
+  // Here we use props.title and props.amount but we have to pass forward props.date to ExpenseDate
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
