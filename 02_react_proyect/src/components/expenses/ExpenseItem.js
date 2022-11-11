fb45c97d1c2f24b3af3b88fe2b65993e54e640a1
Carlos Fuentes <char_basket@hotmail.com>
2022-11-11 16:38:57 +0100

    Add 02_react_project

---
+++ 02_react_proyect/src/components/expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../ui/Card';
+
+const ExpenseItem = (props) => {
+  // It´s always better to have the logic outside the return.
+  // It´s better to make another component
+
+  // Here we use props.title and props.amount but we have to pass forward props.date to ExpenseDate
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
