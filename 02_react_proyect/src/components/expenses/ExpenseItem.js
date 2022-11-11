7b8e7720c39315cb2df38c1d9bb2c807c82d503e
Carlos Fuentes <char_basket@hotmail.com>
2022-11-11 16:48:47 +0100

    First Event Listener

--- 02_react_proyect/src/components/expenses/ExpenseItem.js
+++ 02_react_proyect/src/components/expenses/ExpenseItem.js
@@ -6,6 +6,10 @@ const ExpenseItem = (props) => {
   // It´s always better to have the logic outside the return.
   // It´s better to make another component
 
+  const clickHandler = () => {
+    console.log('Clicked!!');
+  };
+
   // Here we use props.title and props.amount but we have to pass forward props.date to ExpenseDate
   return (
     <Card className="expense-item">
@@ -14,6 +18,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
