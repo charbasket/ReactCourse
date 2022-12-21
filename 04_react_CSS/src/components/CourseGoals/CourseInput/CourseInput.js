fd602af33e803b4e176f854355406ab0494e1df1
Carlos Fuentes <char_basket@hotmail.com>
2022-12-21 10:30:06 +0100

    Styled-Components and CSS Modules

--- 04_react_CSS/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 04_react_CSS/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,27 +1,102 @@
 import React, { useState } from 'react';
-
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const CourseInput = props => {
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? 'red' : 'black')};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     /* border: 1px solid #ccc; */
+//     border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
+//     background: ${(props) => (props.invalid ? '#ffd7d7' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+//   /* &.invalid input {
+//     border-color: red;
+//     background: #ffd7d7;
+//   } */
+
+//   /* &.invalid label {
+//     color: red;
+//   } */
+// `;
+
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* You can use inline styles like this but is not the best
+       *
+       * <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+       *
+       * It´s better to add classes dynamically
+       *
+       */}
+
+      {/* We can create this with styled-components */}
+      {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type='text'
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+        />
+      </div> */}
+
+      {/* Use comments if we want to use styled-components */}
+      {/* <FormControl className={!isValid ? 'invalid' : ''}> */}
+      {/* <FormControl invalid={!isValid}> */}
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
+        <label>Course Goal</label>
+        <input
+          type='text'
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      {/* </FormControl> */}
+
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
