7f03e67be0b8344b366840bc71d684b0ac45606f
Carlos Fuentes <char_basket@hotmail.com>
2022-12-21 16:53:34 +0100

    Fragments and portals

--- 06_react_fragments_portals_refs/src/components/UI/ErrorModal.js
+++ 06_react_fragments_portals_refs/src/components/UI/ErrorModal.js
@@ -1,25 +1,67 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+// We split this into two components to use Portals
+// const ErrorModal = (props) => {
+//   return (
+//     <>
+//       <div className={classes.backdrop} onClick={props.onConfirm} />;
+//       <Card className={classes.modal}>
+//         <header className={classes.header}>
+//           <h2>{props.title}</h2>
+//         </header>
+//         <div className={classes.content}>
+//           <p>{props.message}</p>
+//         </div>
+//         <footer className={classes.actions}>
+//           <Button onClick={props.onConfirm}>Okay</Button>
+//         </footer>
+//       </Card>
+//     </>
+//   );
+// };
+
+//ReactDOM.createPortal ( First the element we want to portal, Second where we want to portal (we need to create divs on index.html on public folder))
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </>
   );
 };
 
